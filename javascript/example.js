6cd195674c40ef64a1581843d4ae51d91ccb7f13
justin leggett <justin@codingrookie.com>
2016-10-03 09:38:45 -0500

    completed the exercise

---
+++ javascript/example.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function Animal () {
+
+}
+
+function Tenodera () {
+  this.genusName = null;
+}
+// Set Animal as the prototype of Tenodera
+
+function TenoderaSinensis () {
+  this.speciesName = null;
+}
+// Set Tenodera as the prototype for TenoderaSinensis
+
+// Create a new instance of TenoderaSinensis
+
+// Output the name, genus name, and species name for your TenoderaSinensis
---
+++ javascript/zoo.js
@@ -0,0 +1,59 @@
+"use strict";
+/*
+http://a-z-animals.com/reference/animal-classification/
+Example 1 - Tiger
+Kingdom: Animalia (Animal)
+Phylum: Chordata (Vertebrate)
+Class: Mammalia (Mammal)
+Order: Carnivora (Carnivore)
+Family: Felidae (Cat)
+Genus: Panthera
+Species: Panthera tigris (Tiger)
+*/
+// Output div
+var $output = $('#output');
+
+function Kingdom() {
+    this.kingdom = "Animal";
+}
+
+function Phylum() {
+    this.phylum = "Vertebrate";
+}
+Phylum.prototype = new Kingdom();
+
+function Animal () {
+    this.animal = null;  // tiger
+}
+Animal.prototype = new Phylum();
+
+function Tenodera () {
+  this.genusName = null; // cat
+}
+Tenodera.prototype = new Animal();
+
+function TenoderaSinensis () {
+  this.speciesName = null; // Panthera Tigris
+}
+TenoderaSinensis.prototype = new Tenodera();
+
+// Create a new instance of TenoderaSinensis
+var tiger = new TenoderaSinensis();
+tiger.animal = "tiger";
+tiger.genusName = "cat";
+tiger.speciesName = "Panthera Tigris";
+
+var orangutan = new TenoderaSinensis();
+orangutan.animal = "Orangutan";
+orangutan.genusName = "Pongo";
+orangutan.speciesName = "Pongo pygmaeus";
+
+// Output the name, genus name, and species name for your TenoderaSinensis
+// var outputHTML = `<div>The animal is a <u>${tiger.animal}</u> his genus is <u>${tiger.genusName}</u> and his species name is <u>${tiger.speciesName}</u>.</div>`;
+var outputHTML = `<div>The animal is a <u>${orangutan.animal}</u> his genus is <u>${orangutan.genusName}</u> and his species name is <u>${orangutan.speciesName}</u>.</div>`;
+
+$output.html(outputHTML);
+console.log("tiger", tiger);
+// console.log("tiger", tiger);
+
+
